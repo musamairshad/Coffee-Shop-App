8dd87c33985f2741764b0c5206c2fcef2a129939
Usama Irshad <imusamairshad@gmail.com>
2024-06-27 09:05:19 -0700

    modified.

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -3,5 +3,15 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 
 class FavoritesProvider with ChangeNotifier {
-  final List<Product> favoriteProducts = [];
+  static final List<Product> favoriteProducts = [];
+
+  void toggleFavoriteStatus(Product product) {
+    product.isFavorite = !product.isFavorite;
+    if (product.isFavorite) {
+      favoriteProducts.add(product);
+    } else {
+      favoriteProducts.remove(product);
+    }
+    notifyListeners();
+  }
 }
